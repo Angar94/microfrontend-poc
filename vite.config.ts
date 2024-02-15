0dcf0d4cccefeb82d4fff3a411654db566ffe428
Andres Garcia <andres.garcia@loadsure.net>
2024-02-15 16:44:33 +0000

    feat: base on vite.config.ts from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// To get version directly from the package.json
+import * as pack from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'v1.2.0',
+  base: `v${pack.version}`,
 })
